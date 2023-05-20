aa24405ae18708ccb0ee736a1251eef10a68109d
FernandoManeiro <maneiro.hide@gmail.com>
2023-05-20 12:29:08 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import 'styled-components'
+import DataTable, {createTheme} from 'react-data-table-component';
 
-function App() {
+const App= ()=> {
+
+  //1- Configurar los hooks
+    const [users, setUsers]= useState([])
+
+  //2- funcion para mostrar los datos con fetch
+    const showData= async ()=>{
+        const resp= await fetch('https://gorest.co.in/public/v2/users')
+        const data = await resp.json()
+        console.log(data);
+        setUsers(data)
+    }
+
+    useEffect(()=>{
+      showData();
+    }, [])
+
+  //3- configurar los columns para datatable
+
+  const columns=[
+    {
+      name:'ID',
+      selector:row=> row.id
+    },
+    {
+      name:'NAME',
+      selector:row=> row.name
+    },
+    {
+      name:'E_MAIL',
+      selector:row=> row.email
+    }
+  ]
+
+
+  //4- mostramo la data en datatable
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Api Fetch React</h1>
+      <DataTable 
+        columns={columns}
+        data={users}
+        pagination
+      />
     </div>
   );
 }
